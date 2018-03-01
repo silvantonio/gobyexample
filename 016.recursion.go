e448662340305dd8fe17247649807773eb2d9e53
Antonio Silva <a.silva@pointerbp.nl>
2018-03-01 18:25:59 +0100

    exercises 016 - 019

--- 016.recursion.go
+++ 016.recursion.go
@@ -1,1 +1,15 @@
 package main
+
+import "fmt"
+
+func fact(n int) int {
+	if n == 0 {
+		return 1
+	}
+	fmt.Println("n:", n)
+	return n * fact(n-1)
+}
+
+func main() {
+	fmt.Println(fact(7))
+}
---
+++ 017.pointers.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func zeroval(ival int) {
+	ival = 0
+}
+
+func zeroptr(iptr *int)  {
+	fmt.Println("pointer in zeroptr:", &*iptr)
+	*iptr = 0
+}
+
+func main()  {
+	i := 1
+	fmt.Println("initial:", i)
+
+	zeroval(i)
+	fmt.Println("zeroval:", i)
+
+	zeroptr(&i)
+	fmt.Println("zeroval:", i)
+
+	fmt.Println("pointer:", &i)
+}
